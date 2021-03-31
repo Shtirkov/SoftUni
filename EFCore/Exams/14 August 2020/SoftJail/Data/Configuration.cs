f892600d73c5130bd92bf62c0c0aece43b1aefc1
Shtirkov <shtirkov96@gmail.com>
2021-03-31 21:10:09 +0300

    EfCoreExamFirst

---
+++ EFCore/Exams/14 August 2020/SoftJail/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftJail.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-52S5USR\SQLEXPRESS;Database=SoftJail;Trusted_Connection=True";
+    }
+}

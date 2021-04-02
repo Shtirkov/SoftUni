6aa48af5ce68518312bd2ba9c386d10f3fe4e652
Shtirkov <shtirkov96@gmail.com>
2021-04-02 21:38:02 +0300

    Exam08August

---
+++ EFCore/07. Exams/08 Augist 2020/VaporStore/Data/Models/GameTag.cs
@@ -0,0 +1,18 @@
+﻿namespace VaporStore.Data.Models
+{
+    public class GameTag
+    {
+        //•	GameId – integer, Primary Key, foreign key(required)
+        //•	Game – Game
+        //•	TagId – integer, Primary Key, foreign key(required)
+        //•	Tag – Tag
+
+        public int GameId { get; set; }
+
+        public Game Game { get; set; }
+
+        public int TagId { get; set; }
+
+        public Tag Tag { get; set; }
+    }
+}

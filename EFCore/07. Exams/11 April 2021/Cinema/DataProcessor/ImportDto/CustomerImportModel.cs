425d9053327b7cb7bfe57f3817e9ac6dc17c959b
Shtirkov <shtirkov96@gmail.com>
2021-04-11 17:22:53 +0300

    11April2021

---
+++ EFCore/07. Exams/11 April 2021/Cinema/DataProcessor/ImportDto/CustomerImportModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Cinema.DataProcessor.ImportDto
+{
+    [XmlType("Customer")]
+    public class CustomerImportModel
+    {
+        //  <FirstName>Randi</FirstName>
+        //  <LastName>Ferraraccio</LastName>
+        //  <Age>20</Age>
+        //  <Balance>59.44</Balance>
+        //  <Tickets>
+
+        //•	Id – integer, Primary Key
+        //•	FirstName – text with length[3, 20] (required)
+        //•	LastName – text with length[3, 20] (required)
+        //•	Age – integer in the range[12, 110] (required)
+        //•	Balance - decimal (non-negative, minimum value: 0.01) (required)
+        //•	Tickets - collection of type Ticket
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        [XmlElement("FirstName")]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        [XmlElement("LastName")]
+        public string LastName { get; set; }
+
+        [Range(12, 110)]
+        [XmlElement("Age")]
+        public int Age { get; set; }
+
+        [Range(0.01, double.MaxValue)]
+        [XmlElement("Balance")]
+        public decimal Balance { get; set; }
+
+        [XmlArray]
+        public List<TicketImportModel> Tickets { get; set; }
+    }
+}

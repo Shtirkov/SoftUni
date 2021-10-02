a814df49fd8fa655b5fe28f719336733fb8f9c7e
Shtirkov <shtirkov96@gmail.com>
2021-10-02 13:16:44 +0300

    chatSheet.js added

---
+++ JS-Advanced/05. DOM Manipulations and Events - Exercise/01. Sections/script.js
@@ -0,0 +1,19 @@
+function create(words) {
+   const content = document.getElementById('content');
+  
+  for (const word of words) {
+    const div =  document.createElement('div');
+    const paragraph = document.createElement('p');
+    paragraph.style.display = 'none';
+    paragraph.textContent = word;
+    div.appendChild(paragraph);
+    div.addEventListener('click', onClick);
+    content.appendChild(div);
+  }
+
+  function onClick(ev){
+   const div = ev.target;
+   const paragraph = div.children[0];
+   paragraph.style.display = '';
+  }
+}

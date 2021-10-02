d03de86d1a12bef8685c3fcf5979e3392895f122
Shtirkov <shtirkov96@gmail.com>
2021-10-02 20:49:00 +0300

    EventsExercise

--- JS-Advanced/05. DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
+++ JS-Advanced/05. DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+   [...document.querySelectorAll('.profile button')].forEach(b => b.addEventListener('click', onClick));
+   
+
+   function onClick(ev){
+      const expandCollapseButton = ev.target;
+      const profile = ev.target.parentElement;
+      const hiddenFieldsDiv = [...profile.querySelectorAll('div')].find(d => d.id.includes('HiddenFields'));
+      const unlockButton = profile.querySelector('input[type="radio"][value="unlock"]');
+      
+      if (unlockButton.checked) {
+        if (expandCollapseButton.textContent == 'Show more') {
+            hiddenFieldsDiv.style.display = 'block';
+            expandCollapseButton.textContent = 'Hide it';
+        }else{
+          hiddenFieldsDiv.style.display = 'none';
+          expandCollapseButton.textContent = 'Show more';
+        }
+      }
+   }
 }
--- JS-Advanced/05. DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS-Advanced/05. DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    const select = document.createElement('select')
+    select.id = 'menu';  
+      
+        const text = document.getElementById('newItemText').value;
+        const value = document.getElementById('newItemValue').value;
+        if (text != '' && value != '') {
+            const option = document.createElement('option');
+            option.textContent = text;
+            option.value = value;
+            document.getElementById('menu').appendChild(option);
+            document.getElementById('newItemText').value = '';
+            document.getElementById('newItemValue').value = '';
+        
+    }
 }

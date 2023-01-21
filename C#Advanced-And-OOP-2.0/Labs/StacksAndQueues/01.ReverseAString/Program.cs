8946998e33b21342fdb8cca19a1870f7c8e5ff8e
Shtirkov <shtirkov96@gmail.com>
2023-01-21 11:16:53 +0200

    Stack and queues lab tasks

---
+++ C#Advanced-And-OOP-2.0/Labs/StacksAndQueues/01.ReverseAString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace _01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToCharArray();
+            var stack = new Stack<char>(input);
+            var sb = new StringBuilder();
+
+            foreach (var symbol in stack) 
+            {
+                sb.Append(symbol);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ C#Advanced-And-OOP-2.0/Labs/StacksAndQueues/08.TrafficJam/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _08.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfPassingCars = int.Parse(Console.ReadLine());
+            var car = Console.ReadLine();
+            var carsQueue = new Queue<string>();
+            var counter = 0;
+
+            while (car != "end")
+            {
+                if (car == "green")
+                {
+                    for (int i = 0; i < numberOfPassingCars; i++)
+                    {
+                        if (carsQueue.Count == 0)
+                            break;                        
+
+                        counter++;
+                        Console.WriteLine($"{carsQueue.Dequeue()} passed!");
+                    }
+                    car = Console.ReadLine();
+                }
+                else
+                {
+                    carsQueue.Enqueue(car);
+                    car = Console.ReadLine();
+                }                
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}

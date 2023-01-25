f0db37f106450af40c3a029eae97bdc933281fc3
Shtirkov <shtirkov96@gmail.com>
2023-01-25 07:53:36 +0200

    Stack and queues exercises

---
+++ C#Advanced-And-OOP-2.0/Exercises/StacksAndQueues/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _02.BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var queue = new Queue<int>();
+
+            var elementsToEnqueue = elements[0];
+            var elementsToDequeue = elements[1];
+            var elementToLookFor = elements[2];
+
+            for (int i = 0; i < elementsToEnqueue; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < elementsToDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (queue.Contains(elementToLookFor))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+        }
+    }
+}

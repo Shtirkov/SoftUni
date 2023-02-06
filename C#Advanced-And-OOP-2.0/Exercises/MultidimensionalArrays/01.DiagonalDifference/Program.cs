ff1c3baf49ecaf48a5d2bdb7fdd3daf1d16ccc36
Shtirkov <shtirkov96@gmail.com>
2023-02-06 17:46:13 +0200

    Diagonal Difference

---
+++ C#Advanced-And-OOP-2.0/Exercises/MultidimensionalArrays/01.DiagonalDifference/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixSize = int.Parse(Console.ReadLine());
+            var matrix = new int[matrixSize, matrixSize];
+            var primaryDiagonalSum = 0;
+            var secondaryDiagonalSum = 0;
+            var iterator = 0; 
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                for (int col = iterator; col < matrix.GetLength(1); col++)
+                {
+                    primaryDiagonalSum += input[iterator];
+                    iterator++;
+                    secondaryDiagonalSum += input[matrix.GetLength(1) - iterator];
+                    break;
+                }
+            }
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
+        }
+    }
+}

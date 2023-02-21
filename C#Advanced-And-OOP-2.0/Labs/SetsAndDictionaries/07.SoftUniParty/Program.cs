13a76bdc31398e74f74699c19d1c5722c14b8c71
Shtirkov <shtirkov96@gmail.com>
2023-02-21 22:20:25 +0200

    Softuni party

---
+++ C#Advanced-And-OOP-2.0/Labs/SetsAndDictionaries/07.SoftUniParty/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _07.SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var guest = Console.ReadLine();
+            var vipGuests = new HashSet<string>();
+            var regularGuests = new HashSet<string>();
+
+            while (guest != "PARTY")
+            {
+                if (char.IsDigit(guest[0]))
+                {
+                    vipGuests.Add(guest);
+                }
+                else
+                {
+                    regularGuests.Add(guest);
+                }
+
+                guest = Console.ReadLine();
+            }
+
+            guest = Console.ReadLine();
+
+            while (guest != "END")
+            {
+                if (char.IsDigit(guest[0]))
+                {
+                    vipGuests.Remove(guest);
+                }
+                else
+                {
+                    regularGuests.Remove(guest);
+                }
+
+                guest = Console.ReadLine();
+            }
+
+            Console.WriteLine(vipGuests.Count + regularGuests.Count);
+            vipGuests.ToList().ForEach(x => Console.WriteLine(x));
+            regularGuests.ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}

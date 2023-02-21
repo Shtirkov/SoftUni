1bc7af48f2b1bce6474d57db3f7b68a1a31f5f0f
Shtirkov <shtirkov96@gmail.com>
2023-02-21 21:48:26 +0200

    Parking lot

---
+++ C#Advanced-And-OOP-2.0/Labs/SetsAndDictionaries/07.ParkingLot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace _06.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var carsInTheParkingLot = new HashSet<string>();
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                var inOrOut = input.Split(", ")[0];
+                var carNumber = input.Split(", ")[1];
+
+                if (inOrOut == "IN")
+                {
+                    carsInTheParkingLot.Add(carNumber);
+                }
+                else
+                {
+                    carsInTheParkingLot.Remove(carNumber);
+                }
+
+                input = Console.ReadLine();
+            }
+            
+            if (carsInTheParkingLot.Count > 0)
+            {
+                carsInTheParkingLot.ToList().ForEach(car => Console.WriteLine(car));
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}

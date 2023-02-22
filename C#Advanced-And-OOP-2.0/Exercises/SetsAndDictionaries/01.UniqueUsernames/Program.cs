547710dc161eef0358c903b9f024374159e4f43b
Shtirkov <shtirkov96@gmail.com>
2023-02-22 16:17:14 +0200

    Unique usernames

---
+++ C#Advanced-And-OOP-2.0/Exercises/SetsAndDictionaries/01.UniqueUsernames/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var usernamesCount = int.Parse(Console.ReadLine());
+            var uniqueUsernames = new HashSet<string>();
+
+            for (int i = 0; i < usernamesCount; i++)
+            {
+                uniqueUsernames.Add(Console.ReadLine());
+            }
+
+            uniqueUsernames.ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}

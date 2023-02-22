54e9820d59a2f24822ad4e29ee4857519b8db226
Shtirkov <shtirkov96@gmail.com>
2023-02-22 16:44:22 +0200

    Even times

---
+++ C#Advanced-And-OOP-2.0/Exercises/SetsAndDictionaries/04.EvenTimes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersCount = int.Parse(Console.ReadLine());
+            var numbersDictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                if (!numbersDictionary.ContainsKey(currentNumber))
+                {
+                    numbersDictionary[currentNumber] = 0;
+                }
+                numbersDictionary[currentNumber]++;
+            }
+
+            foreach (var number in numbersDictionary)
+            {
+                if (number.Value % 2 == 0)
+                {
+                    Console.WriteLine(number.Key);
+                }
+            }
+        }
+    }
+}

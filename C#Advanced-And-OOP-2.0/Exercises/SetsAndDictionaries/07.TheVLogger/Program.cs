8b9ee8b98e7020c11df831ab558fc575a6e99f20
Shtirkov <shtirkov96@gmail.com>
2023-02-23 17:41:27 +0200

    Vlogger task in progress. Implemented the functionality for adding vloggers to the platform. Functionality for following other people is still in progress.

---
+++ C#Advanced-And-OOP-2.0/Exercises/SetsAndDictionaries/07.TheVLogger/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace _07.TheVLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var vloggersDictionary = new Dictionary<string, Dictionary<string,string>>();
+
+            while (input != "Statistics")
+            {
+                var inputArray = input.Split();
+                var vlogger = inputArray[0];
+                var command = inputArray[1];
+               
+                switch (command)
+                {
+                    case "joined":
+                        if (!vloggersDictionary.ContainsKey(vlogger))
+                        {
+                            vloggersDictionary.Add(vlogger, new Dictionary<string, string>());
+                        }
+                        break;
+                    case "followed":
+                        var vloggerToFollow = inputArray[2];
+                       
+                        if (vlogger != vloggerToFollow &&
+                            vloggersDictionary.ContainsKey(vlogger) &&
+                            vloggersDictionary.ContainsKey(vloggerToFollow) &&
+                            vloggersDictionary[vlogger].Values.Contains(vloggerToFollow))
+                        {
+                            
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The V-Logger has a titak if {vloggersDictionary.Keys.Count} vloggers in its logs.");
+            var iterator = 1;
+            foreach (var vlogger in vloggersDictionary)
+            {
+                var output = new StringBuilder();
+                output.Append($"{iterator}. {vlogger.Key}: ");
+
+                foreach (var item in vlogger.Value)
+                {
+                    output.Append($"{item.Key} followers, {item.Value} following");
+                }
+                iterator++;
+                Console.WriteLine(output.ToString());
+            }
+        }
+    }
+}

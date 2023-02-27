93abcefd107eee97a9732e69088b79a269a17773
Shtirkov <shtirkov96@gmail.com>
2023-02-27 16:31:17 +0200

    Add line numbers

---
+++ C#Advanced-And-OOP-2.0/Labs/StreamsFilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.LineNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("../../../input.txt"))
+            {
+                var splittedInput = reader.ReadToEnd().Split(Environment.NewLine);
+
+                using (var writer = new StreamWriter("../../../output.txt"))
+                {
+                    for (int i = 0; i < splittedInput.Length - 1; i++)
+                    {
+                        writer.WriteLine($"{i + 1}. {splittedInput[i]}");
+                    }
+                }
+            }
+        }
+    }
+}

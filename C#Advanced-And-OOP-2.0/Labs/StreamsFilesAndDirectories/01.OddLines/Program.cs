e5741da46b79b0d5838a59a545883b3a7ef23cd0
Shtirkov <shtirkov96@gmail.com>
2023-02-27 16:23:01 +0200

    Odd lines

---
+++ C#Advanced-And-OOP-2.0/Labs/StreamsFilesAndDirectories/01.OddLines/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.OddLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("../../../input.txt"))
+            {
+                var splittedInput = reader.ReadToEnd().Split(Environment.NewLine);
+
+                for (int i = 0; i < splittedInput.Length; i++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        Console.WriteLine(splittedInput[i]);
+                    }
+                }                
+            }
+        }
+    }
+}

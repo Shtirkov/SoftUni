f131aad6e1260dcf91af297b7f9dd93f0a6c3790
Shtirkov <shtirkov96@gmail.com>
2023-02-28 19:24:41 +0200

    Copy binary file

---
+++ C#Advanced/Exercises/StreamsFilesAndDirectories/04.CopyBinaryFile/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.CopyBinaryFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var readStream = new FileStream("../../../../Resources/copyMe.png", FileMode.Open, FileAccess.Read);
+            using var writeStream = new FileStream("../../../copied.png", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            var buffer = new byte[1024];
+
+            while (readStream.Position < readStream.Length)
+            {
+                readStream.Read(buffer, 0, buffer.Length);
+                writeStream.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}

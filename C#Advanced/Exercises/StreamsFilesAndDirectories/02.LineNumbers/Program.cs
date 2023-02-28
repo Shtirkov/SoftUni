a9f124f79655cb95332f20de5f15f070dad9c543
Shtirkov <shtirkov96@gmail.com>
2023-02-28 18:40:59 +0200

    Line numbers

---
+++ C#Advanced/Exercises/StreamsFilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace _02.LineNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var textAsArray = File.ReadAllLines("../../../../Resources/text.txt");
+            var output = new StringBuilder();
+
+            for (int i = 0; i < textAsArray.Length; i++)
+            {
+                var lettersCount = 0;
+                var punctuationsCount = 0;
+
+                foreach (var character in textAsArray[i])
+                {
+                    if (char.IsLetter(character))
+                    {
+                        lettersCount++;
+                    }
+                    else if (char.IsPunctuation(character))
+                    {
+                        punctuationsCount++;
+                    }
+                }
+                output.AppendLine($"Line {i + 1}: {textAsArray[i]} ({lettersCount})({punctuationsCount})");
+            }
+            File.WriteAllText("../../../output.txt", output.ToString());
+        }
+    }
+}

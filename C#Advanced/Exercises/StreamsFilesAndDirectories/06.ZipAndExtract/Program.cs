de0d203e6bf61d0f59579011d8aa7445d684c83d
Shtirkov <shtirkov96@gmail.com>
2023-03-01 20:57:56 +0200

    Zip and extract

---
+++ C#Advanced/Exercises/StreamsFilesAndDirectories/06.ZipAndExtract/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Compression;
+
+namespace _06.ZipAndExtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = "../../../../Resources/copyMe.png";
+            var zipPath = "../../../copyMe.zip";
+
+            using var zipper = ZipFile.Open(zipPath, ZipArchiveMode.Update);
+            zipper.CreateEntryFromFile(filePath, Path.GetFileName(zipPath));
+
+            var entry = zipper.GetEntry(Path.GetFileName(zipPath));
+            if (entry != null)
+            {
+                entry.ExtractToFile("../../../copyMe.png", true);
+            }
+        }
+    }
+}

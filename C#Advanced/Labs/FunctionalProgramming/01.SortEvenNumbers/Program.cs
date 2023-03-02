126683693a330da520a775750b1ebcb0b990bd25
Shtirkov <shtirkov96@gmail.com>
2023-03-02 18:44:03 +0200

    Sort even numbers

---
+++ C#Advanced/Labs/FunctionalProgramming/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .Where(x => x % 2 == 0)
+                 .OrderBy(x => x)
+                 .ToList();
+
+            Console.WriteLine(string.Join(", ",numbers));
+        }
+    }
+}

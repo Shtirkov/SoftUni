b16e5e73a9b1c1eb49f65a48140942f8e6afb893
Shtirkov <shtirkov96@gmail.com>
2023-03-03 13:00:14 +0200

    Add VAT

---
+++ C#Advanced/Labs/FunctionalProgramming/04.AddVAT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Channels;
+
+namespace _04.AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(x => x * 1.2m)
+                .ToList()
+                .ForEach(x => Console.WriteLine(x.ToString("f2")));
+        }
+    }
+}

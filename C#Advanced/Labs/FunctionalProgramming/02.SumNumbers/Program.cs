648ecb3c3a0e9acc321badadce79f112bd46667e
Shtirkov <shtirkov96@gmail.com>
2023-03-03 12:39:03 +0200

    Sum numbers

---
+++ C#Advanced/Labs/FunctionalProgramming/02.SumNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _02.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();                
+
+            Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}

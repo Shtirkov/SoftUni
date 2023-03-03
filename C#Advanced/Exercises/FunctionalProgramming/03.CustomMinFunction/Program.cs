45dd3c39f340f395961154064817af3376afba1c
Shtirkov <shtirkov96@gmail.com>
2023-03-03 21:25:17 +0200

    Custom min function

---
+++ C#Advanced/Exercises/FunctionalProgramming/03.CustomMinFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<List<int>, int> getSmallestNumber = GetSmallestNumber;
+            Func<List<int>> parseInput = ParseInput;
+            Console.WriteLine(getSmallestNumber.Invoke(parseInput.Invoke()));
+        }
+
+        private static int GetSmallestNumber(List<int> numbers)
+            => numbers.Min();
+
+        private static List<int> ParseInput()
+            => Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+    }
+}

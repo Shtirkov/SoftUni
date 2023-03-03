50a5657c6598ff4cbffc8763a771859137319622
Shtirkov <shtirkov96@gmail.com>
2023-03-03 21:09:36 +0200

    Action point

---
+++ C#Advanced/Exercises/FunctionalProgramming/01.ActionPoint/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01.ActionPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<List<string>> printer = PrintNames;
+            printer.Invoke(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList());
+        }
+
+        private static void PrintNames(List<string> names)
+            => names.ForEach(x => Console.WriteLine(x));
+    }
+}

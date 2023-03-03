7135327453feb033cdcc315674f95c7d6d69f414
Shtirkov <shtirkov96@gmail.com>
2023-03-03 21:17:25 +0200

    Knights of honor

---
+++ C#Advanced/Exercises/FunctionalProgramming/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _02.KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<List<string>> action = AddPrefix;
+            action.Invoke(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToList());
+        }
+
+        private static void AddPrefix(List<string> names)
+            => names.Select(x => $"Sir {x}").ToList().ForEach(x => Console.WriteLine(x));
+    }
+}

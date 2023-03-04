1e0b049affdd5e31020e180d1a3c8dfdacd89be9
Shtirkov <shtirkov96@gmail.com>
2023-03-04 20:08:06 +0200

    Predicate for names

---
+++ C#Advanced/Exercises/FunctionalProgramming/07.PredicateForNames/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var length = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<List<string>, int, List<string>> getNames = GetNames(names, length);
+            names = getNames(names, length);
+
+            names.ForEach(x => Console.WriteLine(x));
+        }
+
+        private static Func<List<string>, int, List<string>> GetNames(List<string> names, int lenght)
+        {
+            Func<List<string>, int, List<string>> func = (List, length) =>
+            {
+                return names.Where(x => x.Length <= lenght).ToList();
+            };
+
+            return func;
+        }
+    }
+}

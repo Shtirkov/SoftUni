c7ba0ea1d0fb878e5f291d820a77aeeabfafddae
Shtirkov <shtirkov96@gmail.com>
2023-03-04 16:00:51 +0200

    Applied arithmetics

---
+++ C#Advanced/Exercises/FunctionalProgramming/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _05.AppliedArithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var nextOperation = Console.ReadLine();
+            Func<List<int>, List<int>> operation = PerformOperation(numbers, nextOperation);
+
+            while (nextOperation != "end")
+            {
+
+                if (nextOperation == "print")
+                {
+                    Action<List<int>> printer = PrintResult(numbers);
+                    printer.Invoke(numbers);
+                }
+                else
+                {
+                    numbers = operation.Invoke(numbers);
+                }
+                nextOperation = Console.ReadLine();
+                operation = PerformOperation(numbers, nextOperation);
+            }
+        }
+
+        private static Func<List<int>, List<int>> PerformOperation(List<int> numbers, string operation)
+        {
+            switch (operation)
+            {
+                case "add": return func => numbers.Select(x => x + 1).ToList();
+                case "subtract": return func => numbers.Select(x => x - 1).ToList();
+                case "multiply": return func => numbers.Select(x => x * 2).ToList();
+                default:
+                    return null;
+            }
+        }
+
+        private static Action<List<int>> PrintResult(List<int> numbers)
+            => action => Console.WriteLine(string.Join(" ", numbers));
+    }
+}

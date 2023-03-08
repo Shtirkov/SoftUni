69df0e930da7fddcbe8ae4acf204d08c42fddd76
Shtirkov <shtirkov96@gmail.com>
2023-03-08 17:10:52 +0200

    Constructors added

--- C#Advanced/Labs/DefiningClasses/CarManufacturer/StartUp.cs
+++ C#Advanced/Labs/DefiningClasses/CarManufacturer/StartUp.cs
@@ -4,16 +4,15 @@
     {
         static void Main(string[] args)
         {
-            var car = new Car();
+            var make = Console.ReadLine();
+            var model = Console.ReadLine();
+            var year = int.Parse(Console.ReadLine());
+            var fuelQuantity = double.Parse(Console.ReadLine());
+            var fuelConsumption = double.Parse(Console.ReadLine());
 
-            car.Make = "VW";
-            car.Make = "MK3";
-            car.Year = 1992;
-            car.FuelQuantity = 200;
-            car.FuelConsumption = 10;
-            car.Drive(2000);
-
-            Console.WriteLine(car.WhoAmI());
+            var firstCar = new Car();
+            var secondCar = new Car(make, model, year);
+            var thirdCar = new Car(make,model,year, fuelQuantity, fuelConsumption);
         }
     }
 }

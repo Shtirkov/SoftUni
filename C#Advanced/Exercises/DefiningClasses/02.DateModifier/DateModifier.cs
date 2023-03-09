777b4194c173b626da3d5bf7f48bfb3c77afb390
Shtirkov <shtirkov96@gmail.com>
2023-03-09 17:14:03 +0200

    Date modifier

---
+++ C#Advanced/Exercises/DefiningClasses/02.DateModifier/DateModifier.cs
@@ -0,0 +1,15 @@
+﻿namespace DateModifier
+{
+    public class DateModifier
+    {
+        public int DaysDifference { get; set; }
+
+        public void CalculateDaysDifference(string firstDate, string secondDate)
+        {
+            var firstDateParsed = DateTime.Parse(firstDate);
+            var secondDateParsed = DateTime.Parse(secondDate);
+
+            DaysDifference = Math.Abs((int)(secondDateParsed - firstDateParsed).TotalDays);
+        }
+    }
+}
---
+++ C#Advanced/Exercises/DefiningClasses/02.DateModifier/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace DateModifier
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var firstDate = Console.ReadLine();
+            var secondDate = Console.ReadLine();
+
+            var dateModifier = new DateModifier();
+            dateModifier.CalculateDaysDifference(firstDate, secondDate);
+
+            Console.WriteLine(dateModifier.DaysDifference);
+        }
+    }
+}

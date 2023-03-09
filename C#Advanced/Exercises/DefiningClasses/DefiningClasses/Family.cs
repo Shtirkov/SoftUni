7fc2aa7a147dbf86b9b9da063c79e1de3a658e7a
Shtirkov <shtirkov96@gmail.com>
2023-03-09 16:47:04 +0200

    Family class introduced and GetOldestMember method implemented

---
+++ C#Advanced/Exercises/DefiningClasses/DefiningClasses/Family.cs
@@ -0,0 +1,13 @@
+﻿namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> People { get; set; } = new List<Person>();
+
+        public void AddMember(Person member)
+            => People.Add(member);
+
+        public Person GetOldestMember()
+            => People.OrderByDescending(p => p.Age).FirstOrDefault();
+    }
+}
--- C#Advanced/Exercises/DefiningClasses/DefiningClasses/StartUp.cs
+++ C#Advanced/Exercises/DefiningClasses/DefiningClasses/StartUp.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            var family = new Family();
+            var familyMembersCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < familyMembersCount; i++)
+            {
+                var personInfo = Console.ReadLine().Split();
+                var name = personInfo[0];
+                var age = int.Parse(personInfo[1]);
+
+                family.AddMember(new Person(name,age));
+            }
+
+            var oldestFamilyMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestFamilyMember.Name} {oldestFamilyMember.Age}");
+         }
     }
 }

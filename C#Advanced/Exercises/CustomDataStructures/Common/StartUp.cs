08ee7f0136e2616db24466f9424e455f42f441c6
Shtirkov <shtirkov96@gmail.com>
2023-03-16 17:36:19 +0200

    Brief restructuring of the projects inside the solution

---
+++ C#Advanced/Exercises/CustomDataStructures/Common/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomList
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ C#Advanced/Exercises/CustomDataStructures/CustomList/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomList
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

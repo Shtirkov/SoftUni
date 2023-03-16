e6d578921f861e5da305aa7043a4dcf2dbf4b434
Shtirkov <shtirkov96@gmail.com>
2023-03-16 17:59:45 +0200

    Started implementation of custom list

---
+++ C#Advanced/Exercises/CustomDataStructures/CustomDataStructures/CustomList.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomDataStructures
+{
+    public class CustomList
+    {
+        private const int _initialCapacity = 2;
+        private int[] _items;
+
+        public CustomList()
+        {
+            _items = new int[_initialCapacity];   
+        }
+
+        public int Count { get; private set; }
+
+        public int Indexer { get; set; }
+    }
+}

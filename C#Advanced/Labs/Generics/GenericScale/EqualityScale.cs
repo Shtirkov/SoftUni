6ba3b33706d6f8feeda161ac0ccdb7bacb15f4c1
Shtirkov <shtirkov96@gmail.com>
2023-03-22 19:35:38 +0200

    Generic scale task implemented

---
+++ C#Advanced/Labs/Generics/GenericScale/EqualityScale.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T _first;
+        private T _second;
+
+        public EqualityScale(T first, T second)
+        {
+            _first = first;
+            _second = second;
+        }
+
+        public bool AreEqual() => _first.Equals(_second);
+    }
+}

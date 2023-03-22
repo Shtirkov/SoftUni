e26d973c777e68a970f734e6a9ec8f00c7fbc810
Shtirkov <shtirkov96@gmail.com>
2023-03-22 19:27:40 +0200

    Box task implemented

---
+++ C#Advanced/Labs/Generics/BoxOfT/Box.cs
@@ -0,0 +1,22 @@
+﻿namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> _internalStack;
+
+        public Box()
+        {
+            _internalStack = new Stack<T>();
+        }
+
+        public int Count
+        {
+            get => _internalStack.Count;
+        }
+
+        public void Add(T item) => _internalStack.Push(item);
+
+        public T Remove() => _internalStack.Pop();
+
+    }
+}

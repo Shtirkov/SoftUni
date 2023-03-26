0a2e5c13d346d5c1309237bdd614ff77e8c680a0
Shtirkov <shtirkov96@gmail.com>
2023-03-26 09:56:05 +0300

    Iterators and comparators - Library task

---
+++ C#Advanced/Labs/IteratorsAndComparators/Library/Library.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> _books;
+
+        public Library(params Book[] books)
+        {
+            _books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return _books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}

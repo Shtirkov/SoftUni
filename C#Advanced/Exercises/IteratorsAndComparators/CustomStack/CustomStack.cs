4412720feb5b537531c27dc460d147896ac4e174
Shtirkov <shtirkov96@gmail.com>
2023-03-30 18:50:25 +0300

    Custom stack

---
+++ C#Advanced/Exercises/IteratorsAndComparators/CustomStack/CustomStack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace CustomStack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> _list;
+        private int _index;
+
+        public CustomStack()
+        {
+            _list = new();
+            _index = -1;
+        }
+
+        public void Push(params T[] elements)
+        {
+            elements.ToList().ForEach(element =>
+            {
+                _list.Add(element);
+                _index++;
+            });
+        }
+
+        public T Pop()
+        {
+            if (!_list.Any())
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            var removedElement = _list[_index];
+            _list.RemoveAt(_index);
+            _index--;
+
+            return removedElement;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = _index; i >= 0; i--)
+            {
+                yield return _list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ C#Advanced/Exercises/IteratorsAndComparators/CustomStack/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace CustomStack
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new CustomStack<int>();
+
+
+            while (true)
+            {
+                var inputArr = Console.ReadLine().Replace(',', ' ').Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (inputArr[0] == "END")
+                {
+                    break;
+                }
+                else if (inputArr[0] == "Push")
+                {
+                    var elements = inputArr.Skip(1).Select(int.Parse).ToList();
+                    elements.ForEach(element => stack.Push(element));
+                }
+                else if (inputArr[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (Exception e)
+                    {
+
+                        Console.WriteLine(e.Message);
+                    }
+                }                                
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

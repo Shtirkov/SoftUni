b86a1587d7f27647a8d8b5f5792ab83e0cb1197e
Shtirkov <shtirkov96@gmail.com>
2023-03-30 18:03:38 +0300

    Listy Iterator

---
+++ C#Advanced/Exercises/IteratorsAndComparators/ListyIterator/ListyIterator.cs
@@ -0,0 +1,36 @@
+﻿namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private readonly List<T> _list;
+        private int _index;
+
+        public ListyIterator(List<T> collection)
+        {
+            _list = collection;
+            _index = 0;
+        }
+
+        public bool HasNext() => _index < _list.Count - 1;
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                _index++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            if (!_list.Any())
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(_list[_index]);
+        }
+    }
+}
---
+++ C#Advanced/Exercises/IteratorsAndComparators/ListyIterator/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var inputArr = Console.ReadLine().Split();
+            var command = inputArr[0];
+            var elements = inputArr.Skip(1).ToList();
+            var collection = new ListyIterator<string>(elements);
+
+            while (command != "END")
+            {
+                switch (command)
+                {
+                    case "HasNext":
+                        Console.WriteLine(collection.HasNext());
+                        break;
+                    case "Move":
+                        Console.WriteLine(collection.Move());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            collection.Print();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}

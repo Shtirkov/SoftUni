013b81553cb56216bf4efbff9113dde522865045
Shtirkov <shtirkov96@gmail.com>
2023-03-31 22:40:29 +0300

    Equality logic

---
+++ C#Advanced/Exercises/IteratorsAndComparators/EqualityLogic/Person.cs
@@ -0,0 +1,43 @@
+﻿namespace EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int CompareTo(Person? other)
+        {
+            var result = Name.CompareTo(other?.Name);
+
+            if (result == 0)
+            {
+                result = Age.CompareTo(other?.Age);
+            }
+
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Age.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Person)
+            {
+                var other = obj as Person;
+                return Name == other?.Name && Age == other.Age;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C#Advanced/Exercises/IteratorsAndComparators/EqualityLogic/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           var numberOfPeople = int.Parse(Console.ReadLine());
+
+            var sortedSet = new SortedSet<Person>();
+            var hashSet = new HashSet<Person>();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                var info = Console.ReadLine().Split();
+
+                var name = info[0];
+                var age = int.Parse(info[1]);
+                var person = new Person(name, age);
+
+                sortedSet.Add(person);
+                hashSet.Add(person);                
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}

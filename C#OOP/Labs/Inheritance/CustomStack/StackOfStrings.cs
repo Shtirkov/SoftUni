d592ebcde4ff0d70b992157ed98710f92a2b33ad
Shtirkov <shtirkov96@gmail.com>
2023-04-19 19:16:40 +0300

    Custom stack task

---
+++ C#OOP/Labs/Inheritance/CustomStack/StackOfStrings.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => Count > 0;
+
+        public Stack<string> AddRange(params string[] elements)
+        {
+            foreach (var element in elements)
+            {
+                Push(element);
+            }
+            return this;
+        }
+    }
+}
---
+++ C#OOP/Labs/Inheritance/CustomStack/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new StackOfStrings();
+            stack.AddRange("ivan", "pesho", "gosho");
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}

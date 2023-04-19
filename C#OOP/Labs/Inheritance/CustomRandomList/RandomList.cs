878fe65c57ab1d50a2e428de6ea20d3d94db7783
Shtirkov <shtirkov96@gmail.com>
2023-04-19 19:08:44 +0300

    Custom random list task

---
+++ C#OOP/Labs/Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            var rand = new Random();
+            var elementToRemove = base[rand.Next(0, Count - 1)];
+            Remove(elementToRemove);
+            return elementToRemove;
+        }
+    }
+}
---
+++ C#OOP/Labs/Inheritance/CustomRandomList/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var list = new RandomList() { "az", "ti", "toi", "tq", "to"};
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}

58a80ec75b139b81555b4ec7cb654ccf15e49fb1
Shtirkov <shtirkov96@gmail.com>
2023-04-20 20:15:56 +0300

    Restaurant task implemented

---
+++ C#OOP/Exercises/Inheritance/Restaurant/Coffee.cs
@@ -0,0 +1,16 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double _coffeeMilliliters = 50;
+        private const decimal _price = 3.50m;
+
+        public Coffee(string name, double caffeine,double milliliters = _coffeeMilliliters, decimal price = _price)
+            : base(name, price,milliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}

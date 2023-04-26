06ce427465d9ed397fc5494f2dff0104db335c85
Shtirkov <shtirkov96@gmail.com>
2023-04-26 17:38:01 +0300

    Calculate box surface, lateral surface and volume

---
+++ C#OOP/Exercises/Encapsulation/Box/Box.cs
@@ -0,0 +1,63 @@
+﻿namespace Box
+{
+    public class Box
+    {
+        private double _length;
+        private double _width;
+        private double _height;
+        private const string ExceptionMessage = "{0} cannot be zero or negative.";
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get => _length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessage, "Length"));
+                }
+                _length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => _width; 
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessage, "Width"));
+                }
+                _width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => _height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessage, "Height"));
+                }
+                _height = value;
+            }
+        }
+
+        public double CalculateSurfaceArea() => 2 * _length * _width + 2 * _length * _height + 2 * _width * _height;     
+
+        public double CalculateLateralSurfaceArea() => 2 * _length * _height + 2 * _width * _height;
+
+        public double CalculateVolume() => _length * _width * _height;
+        
+    }
+}
---
+++ C#OOP/Exercises/Encapsulation/Box/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace Box
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine()); 
+
+            var box = new Box(length,width, height);
+            Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.CalculateVolume():f2}");
+        }
+    }
+}

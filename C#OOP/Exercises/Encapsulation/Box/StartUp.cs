91ae9b0be20e56192db1e29f77b5f4477dd549e7
Shtirkov <shtirkov96@gmail.com>
2023-04-26 18:06:48 +0300

    Encapsulation task reworked

--- C#OOP/Exercises/Encapsulation/Box/StartUp.cs
+++ C#OOP/Exercises/Encapsulation/Box/StartUp.cs
@@ -6,12 +6,19 @@
         {
             var length = double.Parse(Console.ReadLine());
             var width = double.Parse(Console.ReadLine());
-            var height = double.Parse(Console.ReadLine()); 
+            var height = double.Parse(Console.ReadLine());
 
-            var box = new Box(length,width, height);
-            Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():f2}");
-            Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():f2}");
-            Console.WriteLine($"Volume - {box.CalculateVolume():f2}");
+            try
+            {
+                var box = new Box(length, width, height);
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.CalculateVolume():f2}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }

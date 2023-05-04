396cfc406b43c4a9432498beea2b31391a6e0c52
Shtirkov <shtirkov96@gmail.com>
2023-05-04 16:00:18 +0300

    Last of the failing tests fixed and engine pattern implemented for football team generator task

--- C#OOP/Exercises/Encapsulation/FootballTeamGenerator/StartUp.cs
+++ C#OOP/Exercises/Encapsulation/FootballTeamGenerator/StartUp.cs
@@ -1,90 +1,13 @@
-﻿using FootballTeamGenerator.Models;
+﻿using FootballTeamGenerator.Core;
 
 namespace FootballTeamGenerator
 {
     public class StartUp
     {
-        private const string NonExistingTeamExceptionMessage = "Team {0} does not exist.";
-
         static void Main(string[] args)
         {
-            var command = Console.ReadLine();
-            var teams = new List<Team>();
-
-            while (command != "END")
-            {
-                var commandArgs = command.Split(';');
-                var teamName = commandArgs[1];
-
-                switch (commandArgs[0])
-                {
-                    case "Team":
-                        try
-                        {
-                            teams.Add(new Team(teamName));
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                        }
-                        break;
-                    case "Add":
-                        var playerName = commandArgs[2];
-                        var endurance = int.Parse(commandArgs[3]);
-                        var sprint = int.Parse(commandArgs[4]);
-                        var dribble = int.Parse(commandArgs[5]);
-                        var passing = int.Parse(commandArgs[6]);
-                        var shooting = int.Parse(commandArgs[7]);
-
-                        try
-                        {
-                            var player = new Player(playerName, endurance, sprint, dribble, passing, shooting);
-
-                            if (!teams.Any(team => team.Name == teamName))
-                            {
-                                throw new InvalidOperationException(string.Format(NonExistingTeamExceptionMessage,teamName));
-                            }
-
-                            teams.FirstOrDefault(team => team.Name == teamName).AddPlayer(player);
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                        }
-                        break;
-                    case "Remove":
-                        try
-                        {
-                            if (!teams.Any(team => team.Name == teamName))
-                            {
-                                throw new InvalidOperationException(string.Format(NonExistingTeamExceptionMessage, teamName));
-                            }
-
-                            teams.FirstOrDefault(team => team.Name == teamName).RemovePlayer(commandArgs[2]);
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                        }
-                        break;
-                    case "Rating":
-                        try
-                        {
-                            if (!teams.Any(team => team.Name == teamName))
-                            {
-                                throw new InvalidOperationException(string.Format(NonExistingTeamExceptionMessage, teamName));
-                            }
-                            Console.WriteLine(teams.FirstOrDefault(team => team.Name == teamName));
-                        }
-                        catch (Exception e )
-                        {
-                            Console.WriteLine(e.Message);
-                        }                       
-                        break;
-                }
-
-                command = Console.ReadLine();
-            }
+            var engine = new Engine();
+            engine.Run();
         }
     }
 }

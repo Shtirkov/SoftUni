da7788065dc7e024f43affdc34efeee14770a92d
Shtirkov <shtirkov96@gmail.com>
2023-07-05 11:52:20 +0300

    Animals task implemented

---
+++ C#OOP/Labs/Polymorphism/Animals/Models/Animal.cs
@@ -0,0 +1,16 @@
+﻿namespace Animals.Models
+{
+    public abstract class Animal
+    {
+        private string _name;
+        private string _favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            _name = name;
+            _favouriteFood = favouriteFood;
+        }
+
+        public virtual string ExplainSelf() => $"I am {_name} and my fovourite food is {_favouriteFood}";
+    }
+}

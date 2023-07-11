9c954971c515c0590e1bf1df7efb668ca4cf75db
Shtirkov <shtirkov96@gmail.com>
2023-07-11 14:44:46 +0300

    Square root task implemented

---
+++ C#OOP/Labs/ExceptionHandling/SquareRoot/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace SquareRoot
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = uint.Parse(Console.ReadLine());
+                
+                Console.WriteLine($"{Math.Sqrt(number)}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}

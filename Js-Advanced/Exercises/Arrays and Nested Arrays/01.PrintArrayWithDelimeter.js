ccc6ff9160b8f5b2ca75daf4720029667d93e3cb
Shtirkov <shtirkov96@gmail.com>
2024-05-09 09:59:54 +0300

    print array

---
+++ Js-Advanced/Exercises/Arrays and Nested Arrays/01.PrintArrayWithDelimeter.js
@@ -0,0 +1,20 @@
+function printArray(strings, delimeter) {
+    console.log(strings.join(delimeter));
+}
+
+printArray(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-'
+);
+
+printArray(['How about no?',
+    'I',
+    'will',
+    'not',
+    'do',
+    'it!'],
+    '_'
+);
